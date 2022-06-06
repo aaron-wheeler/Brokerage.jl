281afb1d5581841286a3bc8ffafcdefd53666dd1
Documenter.jl <documenter@juliadocs.github.io>
2022-06-06 20:58:27 +0000

    build based on 2c1a889

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Brokerage","category":"page"},{"location":"#Brokerage","page":"Home","title":"Brokerage","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Brokerage.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Brokerage]","category":"page"}]
+}
